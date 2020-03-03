09e1af726e73b74d9be0ee44b18ee61abb441028
mamiko3 <mmurakami722@gmail.com>
2020-03-03 18:35:59 +0900

    Summary(required)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "tweets#index"
+  root to: "roots#index"
   resources :tweets do
     resources :comments, only: :create
     collection do
